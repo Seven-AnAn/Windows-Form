25790324c1581301b768a0b3257bfd5a29bfedd7
Seven-An <76164726+Seven-AnAn@users.noreply.github.com>
2021-01-12 20:15:04 +0800

    create

---
+++ 窗体跳转/Formvisible/Formvisible/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formvisible
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //隐藏当前窗答口
+            this.Visible = false;
+            Form2 f2 = new Form2();
+            //显示Form2
+            f2.ShowDialog();
+            //显示当前窗口 
+            this.Visible = true;
+        }
+    }
+}
---
+++ 窗体跳转/Formvisible/Formvisible/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formvisible
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //隐藏当前窗答口
+            this.Visible = false;
+            Form3 f3 = new Form3();
+            //显示Form2
+            f3.ShowDialog();
+            //显示当前窗口 
+            this.Visible = true;
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
